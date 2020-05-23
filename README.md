93261ce1d728cad847a07e2e4ade17de6b5d2955
nikolarusakov <nikola.rusakov@rusakov.cz>
2020-05-23 23:42:12 +0200

    chore(deno): fmt with Deno, add README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# ZipItems Algorithm
+
+## Purpose
+- zipItems function shuffles generated sequence two into sequence one
+- an output returns a new combination of sequences ending with an item of first sequence regardless of parity or condition
+
+## Technology pick
+- I've picked alternative Node runtime, Deno.js to have type safety,  
+testing module and benchmarking without the need introducing additional packages or libraries
+- Algorithm implementation is purely written in TypeScript
+
+## How to run
+### Local
+install from [here](https://deno.land/#installation)
+- run tests 
+```bash
+deno run test
+```
+
+### Docker
+- TBD
--- node/zipItems-perf.test.ts
+++ node/zipItems-perf.test.ts
@@ -1,15 +1,19 @@
-import {runBenchmarks, BenchmarkTimer, bench} from "https://deno.land/std/testing/bench.ts";
-import {primarySeqGenerator} from "./zipItems.test.ts";
-import {zipItems} from "./zipItems.ts";
+import {
+  runBenchmarks,
+  BenchmarkTimer,
+  bench,
+} from "https://deno.land/std/testing/bench.ts";
+import { primarySeqGenerator } from "./zipItems.test.ts";
+import { zipItems } from "./zipItems.ts";
 
 bench({
-    name: 'zip 1000 items with period of 2 at 1 offset',
-    runs: 100,
-    func(b: BenchmarkTimer): void {
-        b.start();
-        zipItems(primarySeqGenerator(1000), primarySeqGenerator, 1, 2)
-        b.stop();
-    }
+  name: "zip 1000 items with period of 2 at 1 offset",
+  runs: 100,
+  func(b: BenchmarkTimer): void {
+    b.start();
+    zipItems(primarySeqGenerator(1000), primarySeqGenerator, 1, 2);
+    b.stop();
+  },
 });
 
 runBenchmarks();
